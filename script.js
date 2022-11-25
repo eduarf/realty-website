4166e937d58e9647c72599dd7a61e1e3aa7d27ff
eduarf <fraude_f@yahoo.com>
2022-11-25 13:31:59 +0300

    javascript dot

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ const dots = Array.from(carouselNav.children);
 
 
 // Getting the width of our images
-
 const imgWidth = imgs[0].getBoundingClientRect();
-
 const setImgPosition = (img,index) => {
     img.style.left = imgWidth.width * index + "px";
     //               nextImg * 1 + px = ....px;
     //               nextImg * 2 + px = ....*2px;
-}
+};
 
 imgs.forEach(setImgPosition);
 
@@ -22,13 +20,20 @@ const moveToImg = (list, currentImg, targetImg) => {
     list.style.transform = "translateX(-" + targetImg.style.left +")";
     currentImg.classList.remove('current--img');
     targetImg.classList.add('current--img');
-}
+};
+
+const updateDots = (currentDot, targetDot) => { 
+    currentDot.classList.remove('current--img');
+    targetDot.classList.add('current--img');
+};
 
 nextButton.addEventListener('click',(e) => {
     const currentImg = list.querySelector('.current--img');
-    const nextImg = currentImg.nextElementSibling;
-
+    const nextImg = currentImg.nextElementSibling;;
+    const currentDot = carouselNav.querySelector('.current--img');
+    const nextDot = currentDot.nextElementSibling;
     moveToImg(list, currentImg, nextImg);
+    updateDots(currentDot, nextDot);
 });
 
 
@@ -36,6 +41,28 @@ nextButton.addEventListener('click',(e) => {
 prevButton.addEventListener('click',(e) => {
     const currentImg = list.querySelector('.current--img');
     const prevImg = currentImg.previousElementSibling;
-
+    const currentDot = carouselNav.querySelector('.current--img');
+    const prevDot = currentDot.previousElementSibling;
     moveToImg(list, currentImg, prevImg);
+    updateDots(currentDot, prevDot);
+});
+
+// When we click on the Carousel Nav, switch the images
+
+
+carouselNav.addEventListener('click',(e) => {
+    // What dot was clicked on 
+
+    const targetDot = e.target.closest('button');
+    
+    if(!targetDot) return;
+
+    const currentImg = list.querySelector('.current--img');
+    const currentDot = carouselNav.querySelector('.current--img');
+    const targetIndex = dots.findIndex((dot) => dot == targetDot);
+    const targetImg = imgs[targetIndex];
+
+    moveToImg(list, currentImg, targetImg);
+    updateDots(currentDot, targetDot);
+
 });
